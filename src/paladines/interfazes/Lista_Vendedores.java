3e7709a88c9c972761078756d7e4d344e6e6670f
IsaacVilla11 <124637331+IsaacVilla11@users.noreply.github.com>
2023-02-25 20:02:52 -0500

    Creacion de la ventana Registro Comprador

--- src/paladines/interfazes/Lista_Vendedores.java
+++ src/paladines/interfazes/Lista_Vendedores.java
@@ -44,6 +44,11 @@ public class Lista_Vendedores extends javax.swing.JFrame {
         jLabel1.setBorder(new javax.swing.border.MatteBorder(null));
 
         jButton1.setText("REGRESAR");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/paladines/interfazes/community_users_12977.png"))); // NOI18N
 
@@ -141,6 +146,10 @@ public class Lista_Vendedores extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
