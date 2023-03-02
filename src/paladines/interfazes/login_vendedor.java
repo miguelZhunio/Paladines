3a4775fecf3a499b22441e09f3bc67448569dcf4
EduArdo11x-X <kevin.cajeca.est@tecazuay.edu.ec>
2023-03-02 09:42:33 -0500

    Interfaz de login vendedor terminado

--- src/paladines/interfazes/login_vendedor.java
+++ src/paladines/interfazes/login_vendedor.java
@@ -18,7 +18,7 @@ public class login_vendedor extends javax.swing.JFrame {
     public login_vendedor() {
         initComponents();
     }
-
+///
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
